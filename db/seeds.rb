8299182ccc36f7a2617c7e2f6ae72bc0fef55575
Nicholas Bundy <nicholasjaynovak@gmail.com>
2015-02-07 12:46:40 -0500

    updating db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(first_name: 'Becca', last_name: 'Moore', phone_number: '111-222-3333', email: 'beccamoore@gmail.com', street_address: '1866 Bleecker St Apt 2L', city: 'Ridgewood', state: 'NY', zipcode: 11385, password: 'testing', password_confirmation: 'testing')
-User.create(first_name: 'Stefanie', last_name: 'Rogers', phone_number: '111-222-3333', email: 'stefanie.e.rogers@gmail.com', street_address: '1866 Bleecker St Apt 2L', city: 'Ridgewood', state: 'NY', zipcode: 11385, password: 'testing', password_confirmation: 'testing')
-User.create(first_name: 'Kevin', last_name: 'Abrams', phone_number: '111-222-3333', email: 'kevabes@gmail.com', street_address: '1866 Bleecker St Apt 2L', city: 'Ridgewood', state: 'NY', zipcode: 11385, password: 'testing', password_confirmation: 'testing')
 User.create(first_name: 'Nicholas', last_name: 'Bundy', phone_number: '111-222-3333', email: 'nicholasjaybundy@gmail.com', street_address: '1866 Bleecker St Apt 2L', city: 'Ridgewood', state: 'NY', zipcode: 11385, password: 'testing', password_confirmation: 'testing')
-User.create(first_name: 'Celeste', last_name: 'Back', phone_number: '111-222-3333', email: 'celeste@gmail.com', street_address: '1866 Bleecker St Apt 2L', city: 'Ridgewood', state: 'NY', zipcode: 11385, password: 'testing', password_confirmation: 'testing')
