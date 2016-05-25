2f3e8dc1c2344658efec0209dd94944efd02d455
Nicholas Bundy <nicholasjaynovak@gmail.com>
2016-05-24 22:15:44 -0500

    adding Jen as admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   self.has_secure_password()
 
   def is_admin?
-    email == "nicholasjaybundy@gmail.com" || "heieunjun@gmail.com"
+    email == "nicholasjaybundy@gmail.com" || "heieunjun@gmail.com" || "jen@gallerychurch.com"
   end
 
 end
